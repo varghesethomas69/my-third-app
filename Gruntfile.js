95392dc0e5dedac7e68c401ed8cdd17ebf536b45
varghesethomas69 <varghesethomas69@hotmail.com>
2018-02-19 10:46:37 -0600

    grunt related changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
     war: {
       target: {
         options: {
-          war_dist_folder: 'C:\\Users\\vthomas\\Documents\\Varghese\\', /* Folder where to generate the WAR. */
+          war_dist_folder: '', /* Folder where to generate the WAR. */
           war_name: 'my-third-app'                    /* The name fo the WAR file (.war will be the extension) */
         },
         files: [
